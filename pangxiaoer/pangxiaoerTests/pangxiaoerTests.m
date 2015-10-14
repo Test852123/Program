2a09d0d358890e5567f28fc6901d5c084486651e
XiMenChuiNiu <2435612739@qq.com>
2015-10-14 17:04:42 +0800

    新建工程"庞小二最帅"

---
+++ pangxiaoer/pangxiaoerTests/pangxiaoerTests.m
@@ -0,0 +1,34 @@
+//
+//  pangxiaoerTests.m
+//  pangxiaoerTests
+//
+//  Created by Mac on 15/10/14.
+//  Copyright (c) 2015年 zhiyou. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface pangxiaoerTests : XCTestCase
+
+@end
+
+@implementation pangxiaoerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
