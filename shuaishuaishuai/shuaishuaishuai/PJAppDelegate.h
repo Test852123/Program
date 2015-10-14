e5d1dc74a446ebf784d5d0645149e027642496d6
XiMenChuiNiu <2435612739@qq.com>
2015-10-14 16:55:21 +0800

    新建工程"帅帅帅"

---
+++ shuaishuaishuai/shuaishuaishuai/PJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PJAppDelegate.h
+//  shuaishuaishuai
+//
+//  Created by Mac on 15/10/14.
+//  Copyright (c) 2015年 zhiyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
