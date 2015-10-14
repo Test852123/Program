2ef27ec9dcda71653d7bd9e6eca0623cccaf58ab
XiMenChuiNiu <2435612739@qq.com>
2015-10-14 16:05:37 +0800

    新建工程

---
+++ shuai/shuai/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  shuai
+//
+//  Created by Mac on 15/10/14.
+//  Copyright (c) 2015年 zhiyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PJAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PJAppDelegate class]));
+    }
+}
